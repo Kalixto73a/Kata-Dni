995b4bc7e36dc4dc5e492cc3ba3b2c042760db02
Kalixto73 <Kalixto75@gmail.com>
2025-01-27 16:28:09 +0100

    feat:Create Seeder of DniLettersSeeder

---
+++ database/seeders/DniLettersSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DniLettersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $letters = [
+            [
+                'letter' => 'T'
+            ], 
+            [
+                'letter' => 'R'
+            ], 
+            [
+                'letter' => 'W'
+            ], 
+            [
+                'letter' => 'A'
+            ], 
+            [
+                'letter' => 'G'
+            ], 
+            [
+                'letter' => 'M'
+            ], 
+            [
+                'letter' => 'Y'
+            ], 
+            [
+                'letter' => 'F'
+            ], 
+            [
+                'letter' => 'P'
+            ],
+            [
+                'letter' => 'D'
+            ],
+            [
+                'letter' => 'X'
+            ], 
+            [
+                'letter' => 'B'
+            ], 
+            [
+                'letter' => 'N'
+            ], 
+            [
+                'letter' => 'J'
+            ], 
+            [
+                'letter' => 'Z'
+            ], 
+            [
+                'letter' => 'S'
+            ], 
+            [
+                'letter' => 'Q'
+            ], 
+            [
+                'letter' => 'V'
+            ], 
+            [
+                'letter' => 'H'
+            ], 
+            [
+                'letter' => 'L'
+            ], 
+            [
+                'letter' => 'C'
+            ], 
+            [
+                'letter' => 'K'
+            ], 
+            [
+                'letter' => 'E'
+            ]
+        ];
+
+    }
+}
