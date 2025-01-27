4bb09618d18c5e3dac4e3406461a525d95e7b284
Kalixto73 <Kalixto75@gmail.com>
2025-01-27 17:13:18 +0100

    feat:Create Api Controller

---
+++ app/Http/Controllers/Api/DniCalculationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\DniLetter;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DniCalculationController extends Controller
+{
+    public function calculate(Request $request){
+        $validated = $request->validate([
+            'dni_number' => 'required|integer|min:0|max:99999999',
+        ]);
+        $dniNumber = $validated['dni_number'];
+        $remainder = $dniNumber % 23;
+        $letter = DniLetter::find($remainder + 1);
+        if ($letter) {
+            return response()->json([
+                'dni_number' => $dniNumber,
+                'dni_letter' => $letter->letter,
+            ]);
+        } else {
+            return response()->json(['error' => 'Letra no encontrada'], 404);
+        }
+    }
+}
