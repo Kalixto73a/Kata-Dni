0602c364c48c1a5df52851cad58f9f051e074607
Kalixto73 <Kalixto75@gmail.com>
2025-01-27 17:03:05 +0100

    feat:Create Controller Dni Calculator

--- database/seeders/DniLettersSeeder.php
+++ database/seeders/DniLettersSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DniLettersSeeder extends Seeder
 {
@@ -12,77 +13,12 @@ class DniLettersSeeder extends Seeder
      */
     public function run(): void
     {
-        $letters = [
-            [
-                'letter' => 'T'
-            ], 
-            [
-                'letter' => 'R'
-            ], 
-            [
-                'letter' => 'W'
-            ], 
-            [
-                'letter' => 'A'
-            ], 
-            [
-                'letter' => 'G'
-            ], 
-            [
-                'letter' => 'M'
-            ], 
-            [
-                'letter' => 'Y'
-            ], 
-            [
-                'letter' => 'F'
-            ], 
-            [
-                'letter' => 'P'
-            ],
-            [
-                'letter' => 'D'
-            ],
-            [
-                'letter' => 'X'
-            ], 
-            [
-                'letter' => 'B'
-            ], 
-            [
-                'letter' => 'N'
-            ], 
-            [
-                'letter' => 'J'
-            ], 
-            [
-                'letter' => 'Z'
-            ], 
-            [
-                'letter' => 'S'
-            ], 
-            [
-                'letter' => 'Q'
-            ], 
-            [
-                'letter' => 'V'
-            ], 
-            [
-                'letter' => 'H'
-            ], 
-            [
-                'letter' => 'L'
-            ], 
-            [
-                'letter' => 'C'
-            ], 
-            [
-                'letter' => 'K'
-            ], 
-            [
-                'letter' => 'E'
-            ]
-        ];
+        $letters = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
+        foreach ($letters as $letter) {
+            DB::table('dni_letters')->insert([
+                'letter' => $letter,
+            ]);
+        }
     }
 }
