4dbb7a4ce01c52b9a61aa7484ca54d30a3515986
Kalixto73 <Kalixto75@gmail.com>
2025-01-28 10:16:08 +0100

    feat:Install Api Dependecies

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DniCalculationController;
 
-
-Route::get('/calculate-dni', [DniCalculationController::class, 'index'])->name('index');
+Route::get('/dni', [DniCalculationController::class, 'index'])->name('index');
